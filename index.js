51a0d931599aab48f67f88986784af03a4b0e950
Peilike2 <auaenrique@gmail.com>
2021-04-20 22:59:01 +0200

    2 files modified

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Máster Biga data/ Data Science VIU</h1>`;
+
+//Grafico de columnas. Mi 1º gráfico
+var visualization = d3plus
+    .viz()
+    .container("#viz1")
+    .data([
+      {"year": 1991, "name":"alpha", "value": 15},
+      {"year": 1992, "name":"alpha", "value": 20}
+    ])
+    .type("bar")
+    .id("name")
+    .x("year")
+    .y("value")
+    .draw();
